2abd6158d66486fffe76fe20b1949696956768c1
Saman Ruhunage <samanruhunage012@gmail.com>
2026-03-11 19:13:49 +0530

    implment navigation

---
+++ app/component/Header.tsx
@@ -0,0 +1,38 @@
+'use client' 
+
+import Link  from "next/link"; 
+import Image from 'next/image'; 
+import { usePathname } from 'next/navigation';
+import { cn } from "@/lib/utils"
+
+const Header = () => {
+    const pathname = usePathname() ; 
+  return (
+    <header>
+        <div className="main-container inner">
+            <Link href="/" >
+                <Image src="CoinPulse.svg" 
+                    alt="CoinPusle logo" 
+                    width={60} 
+                    height={10} 
+                    className="rounded-full"
+                />
+            </Link>
+            <nav>
+                <Link href="/" className={cn('nav-link', {
+                    'is-active' : pathname === '/',
+                    'is-home' : true 
+                })}>Home</Link>
+                <p>Search Model </p>
+                <Link href="/coins" className={cn('nav-link', {
+                    'is-active' : pathname ==='/coins', 
+                    
+                })}>All Coins</Link>
+            </nav>
+
+        </div>
+    </header>
+  )
+}
+
+export default Header
